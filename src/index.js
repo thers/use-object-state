efb44c168c7f6c44c9452c072372410ea3fe8c5b
Alexander Kukhta <a.kukhta@superjob.ru>
2018-10-30 11:36:04 +0300

    Add support for functions

--- src/index.js
+++ src/index.js
@@ -2,12 +2,26 @@
 // $FlowIssue: thanks to that react type defs are provided as built-ins to flow distribution they're always late
 import { useState } from 'react';
 
-export function useObjectState<T: Object>(initialState: T): [T, ($Shape<T>) => void] {
+type StateInitializerType<T: Object> =
+  | T
+  | () => T;
+
+type StateUpdateType<T: Object> =
+  | $Shape<T>
+  | (T) => $Shape<T>;
+
+type StateUpdaterType<T: Object> = (StateUpdateType<T>) => void;
+
+export function useObjectState<T: Object>(initialState: StateInitializerType<T>): [T, StateUpdaterType<T>] {
   const [state, setStateRaw] = useState(initialState);
 
   const setState = (partialState) => setStateRaw({
     ...state,
-    ...partialState,
+    ...(
+      typeof partialState === 'function'
+        ? partialState(state)
+        : partialState
+    ),
   });
 
   return [state, setState];
